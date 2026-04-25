0ed9778cc68ebfae2dabd4e301e6b94f7781be8f
Miguel <miguel.lopez81@correo.tdea.edu.co>
2026-04-24 20:36:54 -0500

    feat: atender pacientes usando FIFO (dequeue)

--- gestion_turnos_clinica/clinica.py
+++ gestion_turnos_clinica/clinica.py
@@ -1,4 +1,5 @@
 from collections import deque
+from turno import Turno
 
 class Clinica:
     def __init__(self):
@@ -12,3 +13,19 @@ class Clinica:
         # ENQUEUE: se agrega al final de la cola
         self.colas[doctor].append(paciente)
         print(f"{paciente.nombre} entra a la cola del Dr. {doctor.nombre}")
+
+    def atender_paciente(self, doctor):
+        if doctor.disponible and self.colas[doctor]:
+            # DEQUEUE: se saca el primer paciente
+            paciente = self.colas[doctor].popleft()
+
+            doctor.disponible = False
+
+            turno = Turno(paciente, doctor)
+            self.turnos.append(turno)
+
+            turno.mostrar()
+
+            doctor.disponible = True
+        else:
+            print(f"No hay pacientes o el Dr. {doctor.nombre} no está disponible")
