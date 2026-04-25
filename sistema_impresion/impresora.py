418b0b8789b8fffe2aa40f75707bcf27789d3fb3
Miguel <miguel.lopez81@correo.tdea.edu.co>
2026-04-24 20:40:29 -0500

    feat: imprimir con dequeue de cola y push a pila de impresos

--- sistema_impresion/impresora.py
+++ sistema_impresion/impresora.py
@@ -11,3 +11,23 @@ class Impresora:
         # ENQUEUE: entra al final de la cola de pendientes
         self.cola_pendientes.append(documento)
         print(f"Documento '{documento.nombre}' agregado a la cola de impresion")
+
+    def imprimir_siguiente(self):
+        if self.cola_pendientes:
+            # DEQUEUE: sale el primero de la cola (FIFO)
+            documento = self.cola_pendientes.popleft()
+            # PUSH: se guarda en la pila de impresos (LIFO)
+            self.pila_impresos.append(documento)
+            print(f"Imprimiendo: '{documento.nombre}'")
+        else:
+            print("No hay documentos pendientes")
+
+    def reimprimir_ultimo(self):
+        if self.pila_impresos:
+            # POP: se saca el ultimo impreso de la pila
+            documento = self.pila_impresos.pop()
+            print(f"Reimprimiendo: '{documento.nombre}'")
+            # Vuelve a la pila despues de reimprimir
+            self.pila_impresos.append(documento)
+        else:
+            print("No hay documentos impresos para reimprimir")
