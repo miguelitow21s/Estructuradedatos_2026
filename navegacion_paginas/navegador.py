070a2d62d21585010659859d8c060cab90d6ffd6
Miguel <miguel.lopez81@correo.tdea.edu.co>
2026-04-24 20:38:52 -0500

    feat: agregar visita de paginas con push a pila de atras

--- navegacion_paginas/navegador.py
+++ navegacion_paginas/navegador.py
@@ -5,3 +5,12 @@ class Navegador:
         # Pila de historial hacia adelante
         self.pila_adelante = []
         self.pagina_actual = None
+
+    def visitar(self, pagina):
+        # PUSH: la pagina actual pasa a la pila de atras
+        if self.pagina_actual:
+            self.pila_atras.append(self.pagina_actual)
+        self.pagina_actual = pagina
+        # Al visitar una nueva pagina se borra el historial adelante
+        self.pila_adelante.clear()
+        print(f"Visitando: {self.pagina_actual.url}")
