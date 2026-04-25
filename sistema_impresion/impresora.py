9837061c33664846459a0b2976e487687a6b6e07
Miguel <miguel.lopez81@correo.tdea.edu.co>
2026-04-24 20:40:13 -0500

    feat: agregar documentos a la cola de pendientes (enqueue)

--- sistema_impresion/impresora.py
+++ sistema_impresion/impresora.py
@@ -6,3 +6,8 @@ class Impresora:
         self.cola_pendientes = deque()
         # Pila: documentos ya impresos (LIFO - para reimpresion)
         self.pila_impresos = []
+
+    def agregar_documento(self, documento):
+        # ENQUEUE: entra al final de la cola de pendientes
+        self.cola_pendientes.append(documento)
+        print(f"Documento '{documento.nombre}' agregado a la cola de impresion")
